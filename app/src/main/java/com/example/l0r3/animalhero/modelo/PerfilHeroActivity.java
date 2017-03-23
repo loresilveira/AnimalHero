bacccf6bc35fc877bf69ef310bf336d65dd5cf59
loresilveira <lima.lory@gmail.com>
2017-03-23 16:37:08 -0300

    Activity do perfil

---
+++ app/src/main/java/com/example/l0r3/animalhero/modelo/PerfilHeroActivity.java
@@ -0,0 +1,15 @@
+package com.example.l0r3.animalhero.modelo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.l0r3.animalhero.R;
+
+public class PerfilHeroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_hero);
+    }
+}
